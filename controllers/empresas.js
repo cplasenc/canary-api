3028e2e50df8b51b26de534f1cd991aded411980
Cristian Plasencia <plc.cristian@gmail.com>
2021-02-08 10:39:48 +0000

    metodo CREAR - POST

--- controllers/empresas.js
+++ controllers/empresas.js
@@ -1,34 +1,45 @@
+const Empresa = require("../models/Empresa");
+
 //@desc        Consigue todas las empresas
 //@route       GET /api/v1/empresas
 //@access      Public
 exports.getEmpresas = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Muestra todas'});
-}
+  res.status(200).json({ success: true, msg: "Muestra todas" });
+};
 
 //@desc        Consigue una empresas
 //@route       GET /api/v1/empresas/:id
 //@access      Public
 exports.getEmpresa = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Muestra una'});
-}
+  res.status(200).json({ success: true, msg: "Muestra una" });
+};
 
 //@desc        Crea nueva empresas
 //@route       POST /api/v1/empresas
 //@access      Private
-exports.createEmpresa = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Crea'});
-}
+exports.createEmpresa = async (req, res, next) => {
+  try {
+    const empresa = await Empresa.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: empresa,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc        Actualiza una empresa
 //@route       PUT /api/v1/empresas/:id
 //@access      Public
 exports.updateEmpresa = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Actualiza'});
-}
+  res.status(200).json({ success: true, msg: "Actualiza" });
+};
 
 //@desc        Elimina una empresas
 //@route       DELETE /api/v1/empresas/:id
 //@access      Public
 exports.deleteEmpresa = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Elimina'});
-}
+  res.status(200).json({ success: true, msg: "Elimina" });
+};
