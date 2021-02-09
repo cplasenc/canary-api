82f08262bc0b762ecc52b991c1400c0b68842865
Cristian Plasencia <plc.cristian@gmail.com>
2021-02-09 18:11:51 +0000

    mongoose error handling

--- middleware/error.js
+++ middleware/error.js
@@ -4,12 +4,25 @@ const error = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
+  // mongoose error en la id
   if (err.name === "CastError") {
     const message = `Empresa no encontrada con el id ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
 
-  res.status(erros.statusCode || 500).json({
+  //Mongoose duplicate key
+  if(err.code === 11000) {
+      const message = 'Valor duplicado';
+      error = new ErrorResponse(message, 400)
+  }
+
+  //Mongoose validation error
+  if(err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map(val => val.message);
+      error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || "Error del servidor",
   });
