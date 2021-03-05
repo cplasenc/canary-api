b52608034ca0e85183363c15a8459e5bc9e71823
Cristian Plasencia <plc.cristian@gmail.com>
2021-03-05 15:42:57 +0000

    calcula la media de puntuaciones de una actividad concreta

--- models/Opinion.js
+++ models/Opinion.js
@@ -37,4 +37,32 @@ const OpinionSchema = new mongoose.Schema({
 //que solo se pueda enviar una opinion por actividad
 OpinionSchema.index({ actividad: 1, usuario: 1 }, { unique: true });
 
+//método estático para conseguir la puntuación media de una actividad
+OpinionSchema.statics.getPuntuacionMedia = async function (organizadorId) {
+  const miArray = await this.aggregate([
+    {
+      $match: { actividad: actividadId },
+    },
+    {
+      $group: { _id: '$actividad', puntuacionMedia: { $avg: '$puntuacion' } },
+    },
+  ]);
+
+  try {
+    await this.model('Actividad').findByAndUpdate(actividadId, {
+      puntuacionMedia: miArray[0].puntuacionMedia,
+    });
+  } catch (err) {}
+};
+
+//Consigue el precio medio de una organización despues de añadir una actividad
+OpinionSchema.post('save', function () {
+  this.constructor.getPuntuacionMedia(this.actividad);
+});
+
+//Consigue el precio medio de una organización después de eliminar una actividad
+OpinionSchema.post('remove', function () {
+  this.constructor.getPuntuacionMedia(this.actividad);
+});
+
 module.exports = mongoose.model('Opinion', OpinionSchema);
